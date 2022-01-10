fae2990bdc59671605db3321cd9f81e775664ab6
rock <rock.in.the.shell@gmail.com>
2022-01-10 23:10:18 +0900

    First Commit 2022/01/10

---
+++ PuzzleAlert/app/src/main/java/com/activities/AlarmClockActivity.java
@@ -0,0 +1,15 @@
+package com.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.puzzlealert.R;
+
+public class AlarmClockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_clock);
+    }
+}
